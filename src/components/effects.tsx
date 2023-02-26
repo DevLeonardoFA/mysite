60506ab8f516d353c9d64a679e6ea5988713e24e
DevLeonardoFA <leonardo.fernandes456@gmail.com>
2023-02-26 19:29:53 +0100

    New Effect

--- src/components/effects.tsx
+++ src/components/effects.tsx
@@ -10,10 +10,16 @@ export function Effects(){
         </div>
 
         <div className="Effects_cards">
+            <a href="Effects\3DBox\3dbox.html" target="_blank" rel="noopener noreferrer" className="link_effect">Open in a New Tab</a>
             <iframe src="Effects\3DBox\3dbox.html"></iframe>
         </div>
         <div className="Effects_cards">
+            <a href="Effects\Calc\calculadora.html" target="_blank" rel="noopener noreferrer" className="link_effect">Open in a New Tab</a>
             <iframe src="Effects\Calc\calculadora.html"></iframe>
+        </div>
+        <div className="Effects_cards">
+            <a href="Effects\3DCard\3DCard.html" target="_blank" rel="noopener noreferrer" className="link_effect">Open in a New Tab</a>
+            <iframe src="Effects\3DCard\3DCard.html"></iframe>
         </div>
 
     </section>
